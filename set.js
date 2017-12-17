508dae2a196528d57f58bcc546d1ebc4da0ed0d3
Yasser Mahmud <yasser@programifyjs.com>
2017-12-17 08:00:27 +0500

    questions

--- set.js
+++ set.js
@@ -3,23 +3,13 @@ function Set() {
   this.storage = [];
 };
 
-Set.prototype.addToSet = function (val) {
-  this.storage.push(val);
-};
+Set.prototype.addToSet 
 
-Set.prototype.removeFromSet = function(val) {
-  if (this.storage.length) {
-    var index = this.storage.indexOf(val)
-    index !== -1 ? this.storage.splice(index, 1) : ''
-  } 
-}
+Set.prototype.removeFromSet 
 
-Set.prototype.contains = function(val) {
-  return this.storage.includes(val);
-}
+Set.prototype.contains 
+
 
 var mySet = new Set();
-mySet.addToSet(4);
-mySet.addToSet('hello');
 
 
